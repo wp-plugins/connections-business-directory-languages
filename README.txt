ce2897f5bfb6782d56e0d13c36307264e4a3c3ac
shazahm1@hotmail.com <shazahm1@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-10 23:42:19 +0000

    Commit 1.0
    git-svn-id: https://plugins.svn.wordpress.org/connections-business-directory-languages/trunk@986847 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,77 @@
+=== Connections Business Directory Languages ===
+Contributors: shazahm1@hotmail.com
+Donate link: http://connections-pro.com/
+Tags: addresses, address book, addressbook, bio, bios, biographies, business, businesses, business directory, business-directory, business directory plugin, directory plugin, directory widget, church, contact, contacts, connect, connections, directory, directories, hcalendar, hcard, ical, icalendar, image, images, list, lists, listings, member directory, members directory, members directories, microformat, microformats, page, pages, people, profile, profiles, post, posts, plugin, shortcode, staff, user, users, vcard, wordpress business directory, wordpress directory, wordpress directory plugin, wordpress business directory, languages, widget
+Requires at least: 3.8
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Extension for the Connections Business Directory that adds the ability to add languages spoken to an entry.
+
+== Description ==
+
+This is an extension plugin for the [Connections Business Directory Plugin](http://wordpress.org/plugins/connections/) please be sure to install and active it before adding this plugin.
+
+What does this plugin do? It adds the ability to add the languages spoken to entries. The default choices are [the most spoken languages in the world](http://www.nationsonline.org/oneworld/most_spoken_languages.htm). A filter has been provided to allow the choices of languages to be adjusted to suit your needs.
+
+The languages of an entry can optionally be shown as a content block within a template or with the supplied widget.
+
+[Checkout the screenshots.](http://connections-pro.com/add-on/languages/)
+
+Here are other great extensions that enhance your expirence with the Connections Business Directory:
+
+* [Business Hours](http://wordpress.org/plugins/connections-business-directory-hours/)
+* [Toolbar](http://wordpress.org/plugins/connections-toolbar/)
+* [Income Levels](http://wordpress.org/plugins/connections-business-directory-income-levels/)
+* [Education Levels](http://wordpress.org/plugins/connections-business-directory-education-levels/)
+* [Login](http://wordpress.org/plugins/connections-business-directory-login/)
+
+== Installation ==
+
+= Using the WordPress Plugin Search =
+
+1. Navigate to the `Add New` sub-page under the Plugins admin page.
+2. Search for `connections business directory languages`.
+3. The plugin should be listed first in the search results.
+4. Click the `Install Now` link.
+5. Lastly click the `Activate Plugin` link to activate the plugin.
+
+= Uploading in WordPress Admin =
+
+1. [Download the plugin zip file](http://wordpress.org/plugins/connections-business-directory-languages/) and save it to your computer.
+2. Navigate to the `Add New` sub-page under the Plugins admin page.
+3. Click the `Upload` link.
+4. Select Connections Business Directory Languages zip file from where you saved the zip file on your computer.
+5. Click the `Install Now` button.
+6. Lastly click the `Activate Plugin` link to activate the plugin.
+
+= Using FTP =
+
+1. [Download the plugin zip file](http://wordpress.org/plugins/connections-business-directory-languages/) and save it to your computer.
+2. Extract the Connections Business Directory Languages zip file.
+3. Create a new directory named `connections-business-directory-languages` directory in the `../wp-content/plugins/` directory.
+4. Upload the files from the folder extracted in Step 2.
+4. Activate the plugin on the Plugins admin page.
+
+== Frequently Asked Questions ==
+
+None yet....
+
+== Screenshots ==
+
+[Screenshots can be found here.](http://connections-pro.com/add-on/languages/)
+
+1. The metabox containging the list of languages that can be assigned to a directory entry.
+2. The widget output on the frontend of the site when viewing a single directory entry.
+
+== Changelog ==
+
+= 1.0 09/02/2014 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial release.
---
+++ connections_languages.php
@@ -0,0 +1,317 @@
+<?php
+/**
+ * An extension for the Connections plugin which adds a metabox for languages.
+ *
+ * @package   Connections Education Levels
+ * @category  Extension
+ * @author    Steven A. Zahm
+ * @license   GPL-2.0+
+ * @link      http://connections-pro.com
+ * @copyright 2014 Steven A. Zahm
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Connections Languages
+ * Plugin URI:        http://connections-pro.com
+ * Description:       An extension for the Connections plugin which adds a metabox for languages.
+ * Version:           1.0
+ * Author:            Steven A. Zahm
+ * Author URI:        http://connections-pro.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       connections_lanugages
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if ( ! class_exists('Connections_Languages') ) {
+
+	class Connections_Languages {
+
+		public function __construct() {
+
+			self::defineConstants();
+			self::loadDependencies();
+
+			// register_activation_hook( CNIL_BASE_NAME . '/connections_languages.php', array( __CLASS__, 'activate' ) );
+			// register_deactivation_hook( CNIL_BASE_NAME . '/connections_languages.php', array( __CLASS__, 'deactivate' ) );
+
+			/*
+			 * Load translation. NOTE: This should be ran on the init action hook because
+			 * function calls for translatable strings, like __() or _e(), execute before
+			 * the language files are loaded will not be loaded.
+			 *
+			 * NOTE: Any portion of the plugin w/ translatable strings should be bound to the init action hook or later.
+			 */
+			add_action( 'init', array( __CLASS__ , 'loadTextdomain' ) );
+
+			// Register the metabox and fields.
+			add_action( 'cn_metabox', array( __CLASS__, 'registerMetabox') );
+
+			// Add the business hours option to the admin settings page.
+			// This is also required so it'll be rendered by $entry->getContentBlock( 'languages' ).
+			add_filter( 'cn_content_blocks', array( __CLASS__, 'settingsOption') );
+
+			// Add the action that'll be run when calling $entry->getContentBlock( 'languages' ) from within a template.
+			add_action( 'cn_output_meta_field-languages', array( __CLASS__, 'block' ), 10, 4 );
+
+			// Register the widget.
+			add_action( 'widgets_init', create_function( '', 'register_widget( "CN_Languages_Widget" );' ) );
+		}
+
+		/**
+		 * Define the constants.
+		 *
+		 * @access  private
+		 * @static
+		 * @since  1.0
+		 * @return void
+		 */
+		private static function defineConstants() {
+
+			define( 'CNLANG_CURRENT_VERSION', '1.0.2' );
+			define( 'CNLANG_DIR_NAME', plugin_basename( dirname( __FILE__ ) ) );
+			define( 'CNLANG_BASE_NAME', plugin_basename( __FILE__ ) );
+			define( 'CNLANG_PATH', plugin_dir_path( __FILE__ ) );
+			define( 'CNLANG_URL', plugin_dir_url( __FILE__ ) );
+		}
+
+		/**
+		 * The widget.
+		 *
+		 * @access private
+		 * @since  1.0
+		 * @static
+		 * @return void
+		 */
+		private static function loadDependencies() {
+
+			require_once( CNLANG_PATH . 'includes/class.widgets.php' );
+		}
+
+
+		public static function activate() {
+
+
+		}
+
+		public static function deactivate() {
+
+		}
+
+		/**
+		 * Load the plugin translation.
+		 *
+		 * Credit: Adapted from Ninja Forms / Easy Digital Downloads.
+		 *
+		 * @access private
+		 * @since  1.0
+		 * @static
+		 * @uses   apply_filters()
+		 * @uses   get_locale()
+		 * @uses   load_textdomain()
+		 * @uses   load_plugin_textdomain()
+		 * @return void
+		 */
+		public static function loadTextdomain() {
+
+			// Plugin's unique textdomain string.
+			$textdomain = 'connections_languages';
+
+			// Filter for the plugin languages folder.
+			$languagesDirectory = apply_filters( 'connections_languages_lang_dir', CNLANG_DIR_NAME . '/languages/' );
+
+			// The 'plugin_locale' filter is also used by default in load_plugin_textdomain().
+			$locale = apply_filters( 'plugin_locale', get_locale(), $textdomain );
+
+			// Filter for WordPress languages directory.
+			$wpLanguagesDirectory = apply_filters(
+				'connections_languages_wp_lang_dir',
+				WP_LANG_DIR . '/connections-languages/' . sprintf( '%1$s-%2$s.mo', $textdomain, $locale )
+			);
+
+			// Translations: First, look in WordPress' "languages" folder = custom & update-secure!
+			load_textdomain( $textdomain, $wpLanguagesDirectory );
+
+			// Translations: Secondly, look in plugin's "languages" folder = default.
+			load_plugin_textdomain( $textdomain, FALSE, $languagesDirectory );
+		}
+
+		/**
+		 * Defines the language options.
+		 *
+		 * Default list is the most spoken lanuages in the world.
+		 * @url http://www.nationsonline.org/oneworld/most_spoken_languages.htm
+		 * @url http://www.nationsonline.org/oneworld/languages.htm
+		 *
+		 * @access private
+		 * @since  1.0
+		 * @static
+		 * @uses   apply_filters()
+		 * @return array An indexed array containing the languages.
+		 */
+		private static function languages() {
+
+			$options = array(
+				'ara' => __( 'Arabic', 'connections_languages'),
+				'ben' => __( 'Bengali', 'connections_languages'),
+				'chi' => __( 'Chinese', 'connections_languages'),
+				'eng' => __( 'English', 'connections_languages'),
+				'fra' => __( 'French', 'connections_languages'),
+				'ger' => __( 'German', 'connections_languages'),
+				'hin' => __( 'Hindi', 'connections_languages'),
+				'ita' => __( 'Italian', 'connections_languages'),
+				'jpn' => __( 'Japanese', 'connections_languages'),
+				'kor' => __( 'Korean', 'connections_languages'),
+				'por' => __( 'Portuguese', 'connections_languages'),
+				'rus' => __( 'Russian', 'connections_languages'),
+				'spa' => __( 'Spanish', 'connections_languages'),
+				'tai' => __( 'Tai-Kadai', 'connections_languages'),
+				'vie' => __( 'Vietnamese', 'connections_languages'),
+			);
+
+			return apply_filters( 'cn_languages_options', $options );
+		}
+
+		/**
+		 * Return the language based on the supplied key (ISO 639-2, the alpha-3 code).
+		 *
+		 * @access private
+		 * @since  1.0
+		 * @static
+		 * @uses   levels()
+		 * @param  string $code  The key of the language to return.
+		 * @return mixed         bool | string	The language if found, if not, FALSE.
+		 */
+		private static function language( $code = '' ) {
+
+			if ( ! is_string( $code ) || empty( $code ) || $code === '-1' ) {
+
+				return FALSE;
+			}
+
+			$languages = self::languages();
+			$language  = isset( $languages[ $code ] ) ? $languages[ $code ] : FALSE;
+
+			return $language;
+		}
+
+		/**
+		 * Registered the custom metabox.
+		 *
+		 * @access private
+		 * @since  1.0
+		 * @static
+		 * @uses   levels()
+		 * @uses   cnMetaboxAPI::add()
+		 * @return void
+		 */
+		public static function registerMetabox() {
+
+			$atts = array(
+				'name'     => 'Languages',
+				'id'       => 'languages',
+				'title'    => __( 'Languages', 'connections_languages' ),
+				'context'  => 'side',
+				'priority' => 'core',
+				'fields'   => array(
+					array(
+						'id'      => 'languages',
+						'type'    => 'checkboxgroup',
+						'options' => self::languages(),
+						'default' => '',
+						),
+					),
+				);
+
+			cnMetaboxAPI::add( $atts );
+		}
+
+		/**
+		 * Add the custom meta as an option in the content block settings in the admin.
+		 * This is required for the output to be rendered by $entry->getContentBlock().
+		 *
+		 * @access private
+		 * @since  1.0
+		 * @param  array  $blocks An associtive array containing the registered content block settings options.
+		 * @return array
+		 */
+		public static function settingsOption( $blocks ) {
+
+			$blocks['languages'] = 'Languages';
+
+			return $blocks;
+		}
+
+		/**
+		 * Renders the Languages content block.
+		 *
+		 * Called by the cn_meta_output_field-languages action in cnOutput->getMetaBlock().
+		 *
+		 * @access  private
+		 * @since  1.0
+		 * @static
+		 * @uses   esc_attr()
+		 * @uses   language()
+		 * @param  string $id    The field id.
+		 * @param  array  $value The language codes (ISO 639-2, the alpha-3 code).
+		 * @param  array  $atts  The shortcode atts array passed from the calling action.
+		 *
+		 * @return string
+		 */
+		public static function block( $id, $value, $object = NULL, $atts ) {
+
+			echo '<ul class="cn-languages">';
+
+			foreach ( $value as $code ) {
+
+				if ( $language = self::language( $code ) ) {
+
+					printf( '<li class="cn-language cn-%1$s">%2$s</li>', esc_attr( $code ), esc_attr( $language ) );
+				}
+
+			}
+
+			echo '</ul>';
+		}
+
+	}
+
+	/**
+	 * Start up the extension.
+	 *
+	 * @access public
+	 * @since 1.0
+	 *
+	 * @return mixed object | bool
+	 */
+	function Connections_Languages() {
+
+			if ( class_exists('connectionsLoad') ) {
+
+					return new Connections_Languages();
+
+			} else {
+
+				add_action(
+					'admin_notices',
+					 create_function(
+						 '',
+						'echo \'<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Languages.</p></div>\';'
+						)
+				);
+
+				return FALSE;
+			}
+	}
+
+	/**
+	 * Since Connections loads at default priority 10, and this extension is dependent on Connections,
+	 * we'll load with priority 11 so we know Connections will be loaded and ready first.
+	 */
+	add_action( 'plugins_loaded', 'Connections_Languages', 11 );
+
+}
